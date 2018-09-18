0bd9f257bd9b274594d345bce80c43c984f24209
Alexandra Diljá Birkisdóttir <alexandrab18@ru.is>
2018-09-18 12:06:59 +0000

    prufu

--- TileTravellerFunctions.py
+++ TileTravellerFunctions.py
@@ -1,1 +1,13 @@
-#hello bitch
+def move(x,y,txt):
+    if txt == 'n':
+        y +=1
+    elif txt == 's':
+        y -=1
+    elif txt == 'w':
+        x -=1
+    elif txt == 'e':
+        x +=1
+    tile = (x,y)
+    return tile
+out = move(1,1,'n')
+print = (out)
