df4d2cbc8b67b789e3afa5a1c97d844da5e6fe48
Alexandra Diljá Birkisdóttir <alexandrab18@ru.is>
2018-09-18 11:05:32 +0000

    mimir keyrir þessa

--- TileTraveller.py
+++ TileTraveller.py
@@ -5,78 +5,87 @@
 #Þegar staðsetningin er 31 er leik lokið
 
 place = 11
-travel = 'You can travel: '
+travel = 'You can travel:'
 
 while place != 31:
+    direction = 'q'
     if place == 11:
         print(travel,'(N)orth.')
-        direction = input('Directions: ').lower()
-        if direction == 'n':
-            place = 12
-        else:
-            print('Not a valid direction!')
+        while direction not in 'n':
+            direction = input('Direction: ').lower()
+            if direction == 'n':
+                place = 12
+            else:
+                print('Not a valid direction!')
     elif place == 12:
         print(travel,'(N)orth or (E)ast or (S)outh.')
-        direction = input('Directions: ').lower()
-        if direction == 'n':
-            place = 13
-        elif direction == 'e':
-            place = 22
-        elif direction == 's':
-            place = 11
-        else:
-            print('Not a valid direction!')
+        while direction not in 'nes':
+            direction = input('Direction: ').lower()
+            if direction == 'n':
+                place = 13
+            elif direction == 'e':
+                place = 22
+            elif direction == 's':
+                place = 11
+            else:
+                print('Not a valid direction!')
     elif place == 13:
         print(travel,'(E)ast or (S)outh.')
-        direction = input('Directions: ').lower()
-        if direction == 'e':
-            place = 23
-        elif direction == 's':
-            place = 12
-        else:
-            print('Not a valid direction!')
+        while direction not in 'es':
+            direction = input('Direction: ').lower()
+            if direction == 'e':
+                place = 23
+            elif direction == 's':
+                place = 12
+            else:
+                print('Not a valid direction!')
     elif place == 21:
         print(travel,'(N)orth.')
-        direction = input('Directions: ').lower()
-        if direction == 'n':
-            place = 22
-        else:
-            print('Not a valid direction!')
+        while direction not in 'n':
+            direction = input('Direction: ').lower()
+            if direction == 'n':
+                place = 22
+            else:
+                print('Not a valid direction!')
     elif place == 22:
-        print(travel,'(W)est or (S)outh.')
-        direction = input('Directions: ').lower()
-        if direction == 'w':
-            place = 12
-        elif direction == 's':
-            place = 21
-        else:
-            print('Not a valid direction!')
+        print(travel,'(S)outh or (W)est.')
+        while direction not in 'sw':
+            direction = input('Direction: ').lower()
+            if direction == 'w':
+                place = 12
+            elif direction == 's':
+                place = 21
+            else:
+                print('Not a valid direction!')
     elif place == 23:
-        print(travel,'(W)est or (E)ast.')
-        direction = input('Directions: ').lower()
-        if direction == 'e':
-            place = 33
-        elif direction == 'w':
-            place = 13
-        else:
-            print('Not a valid direction!')
+        print(travel,'(E)ast or (W)est.')
+        while direction not in 'ew':
+            direction = input('Direction: ').lower()
+            if direction == 'e':
+                place = 33
+            elif direction == 'w':
+                place = 13
+            else:
+                print('Not a valid direction!')
     elif place == 32:
         print(travel,'(N)orth or (S)outh.')
-        direction = input('Directions: ').lower()
-        if direction == 'n':
-            place = 33
-        elif direction == 's':
-            place = 31
-        else:
-            print('Not a valid direction!')
+        while direction not in 'ns':
+            direction = input('Direction: ').lower()
+            if direction == 'n':
+                place = 33
+            elif direction == 's':
+                place = 31
+            else:
+                print('Not a valid direction!')
     elif place == 33:
-        print(travel,'(W)est or (S)outh.')
-        direction = input('Directions: ').lower()
-        if direction == 'w':
-            place = 23
-        elif direction == 's':
-            place = 32
-        else:
-            print('Not a valid direction!')
+        print(travel,'(S)outh or (W)est.')
+        while direction not in 'sw':
+            direction = input('Direction: ').lower()
+            if direction == 'w':
+                place = 23
+            elif direction == 's':
+                place = 32
+            else:
+                print('Not a valid direction!')
 if place == 31:
         print('Victory!')
